c205fb0298837f42e6a5133ff97aad236e963028
jonwells10 <jonathonwells10@gmail.com>
2018-10-04 19:15:02 -0500

    push to heroku

--- mongo_uri.js
+++ mongo_uri.js
@@ -1,3 +1,3 @@
-// MONGODB_URI: mongodb:
+MONGODB_URI: mongodb:
 
-// 'heroku_xd394bdd:ncntvcs1jnacmt48iaiodvflb7@ds161016.mlab.com:61016/heroku_xd394bdd'
+'heroku_xd394bdd:ncntvcs1jnacmt48iaiodvflb7@ds161016.mlab.com:61016/heroku_xd394bdd'
